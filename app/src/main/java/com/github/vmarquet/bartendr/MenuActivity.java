0f69606f2d810ff13b600901bc367bf1957bff0f
vmarquet <vincent.marquet1@free.fr>
2014-12-09 14:04:33 +0100

    add ListView to see the menu categories (mais bugs chelou genre pas moyen d'arriver à centrer le TextView  qui sert de titre à cette page)

--- app/src/main/java/com/github/vmarquet/bartendr/MenuActivity.java
+++ app/src/main/java/com/github/vmarquet/bartendr/MenuActivity.java
@@ -3,11 +3,53 @@ package com.github.vmarquet.bartendr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MenuActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ListView lv = (ListView)findViewById(R.id.listViewMenu);
+
+        final String[] liste = {"Bière", "Cocktail", "Vin"};
+        lv.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return liste.length;
+            }
+
+            @Override
+            public String getItem(int i) {
+                return liste[i];
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return i;
+            }  // seriously ?
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                String string = getItem(i);
+
+                // we use the row_menu.xml layout file
+                if(view == null) {
+                    view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_menu, viewGroup, false);
+                }
+
+                // we set the text for each options in the ListView
+                TextView textEdit = (TextView)view.findViewById(R.id.menu_option);
+                textEdit.setText(string);
+
+                return view;
+            }
+        });
     }
 }
